60cf609685fecc87f9571765ba3f10b0c16a9c67
Tomer Negad <tomernegad@gmail.com>
2025-02-23 10:14:00 +0200

    Add fruit class to generate and draw fruit on the screen

--- snake.py
+++ snake.py
@@ -1,4 +1,15 @@
-import pygame,sys
+import pygame,sys,random
+from pygame.math import Vector2
+
+class FRUIT:
+    def __init__(self):
+        self.x = random.randint(0,cell_number - 1)
+        self.y = random.randint(0,cell_number -1)
+        self.pos = Vector2(self.x,self.y)
+        
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x * cell_size,self.pos.y * cell_size,cell_size,cell_size)
+        pygame.draw.rect(screen,(239,11,11),fruit_rect)
 
 cell_size = 30
 cell_number = 20
@@ -6,12 +17,16 @@ cell_number = 20
 screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
 clock = pygame.time.Clock()
 
+fruit = FRUIT()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-            
+
+          
     screen.fill((175,215,70))
+    fruit.draw_fruit() 
     pygame.display.update()
     clock.tick(60)
