3c34178ceeb440c38d0f3ef98ed2ac40843352fa
Tomer Negad <tomernegad@gmail.com>
2025-02-23 09:40:44 +0200

    Made the display with an option t o close the app when pressing X

--- snake.py
+++ snake.py
@@ -1,9 +1,12 @@
-import pygame,sys,random
+import pygame,sys
 
-
-
-pygame.init()
 cell_size = 30
 cell_number = 20
+
+screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
+
 while True:
-    screen = pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
